ac85e0bc19965791a2a2f789f7c727ab0228fa4a
Dianapita444 <dianapita04@upi.edu>
2025-05-05 22:56:11 +0700

    Add files via upload

---
+++ data/GerbangTol_20.js
@@ -0,0 +1,13 @@
+var json_GerbangTol_20 = {
+"type": "FeatureCollection",
+"name": "GerbangTol_20",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "Name": "Gerbang Tol Ciperna Utama", "descriptio": null, "timestamp": null, "begin": null, "end": null, "altitudeMo": null, "tessellate": "-1", "extrude": "0", "visibility": "-1", "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ 108.5337302, -6.7680857 ] } },
+{ "type": "Feature", "properties": { "id": null, "Name": "Gerbang Tol Palimanan 3", "descriptio": null, "timestamp": null, "begin": null, "end": null, "altitudeMo": null, "tessellate": "-1", "extrude": "0", "visibility": "-1", "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ 108.4300156, -6.6896125 ] } },
+{ "type": "Feature", "properties": { "id": null, "Name": "Gerbang Tol Plumbon 3", "descriptio": null, "timestamp": null, "begin": null, "end": null, "altitudeMo": null, "tessellate": "-1", "extrude": "0", "visibility": "-1", "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ 108.4830594, -6.6986798 ] } },
+{ "type": "Feature", "properties": { "id": null, "Name": "Gerbang TOL Ciperna Barat", "descriptio": null, "timestamp": null, "begin": null, "end": null, "altitudeMo": null, "tessellate": "-1", "extrude": "0", "visibility": "-1", "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ 108.529363, -6.7601985 ] } },
+{ "type": "Feature", "properties": { "id": null, "Name": "Gerbang Tol Ciledug 1", "descriptio": null, "timestamp": null, "begin": null, "end": null, "altitudeMo": null, "tessellate": "-1", "extrude": "0", "visibility": "-1", "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ 108.7490048, -6.8882641 ] } },
+{ "type": "Feature", "properties": { "id": null, "Name": "Gerbang Tol Plumbon 4", "descriptio": null, "timestamp": null, "begin": null, "end": null, "altitudeMo": null, "tessellate": "-1", "extrude": "0", "visibility": "-1", "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ 108.4881802, -6.702596 ] } }
+]
+}
